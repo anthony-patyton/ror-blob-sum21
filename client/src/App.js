b6c014b8357e438e318b2043fd4c1582ba4f85aa
John Doe <paytonanthony2020@gmail.com>
2021-06-16 19:45:14 -0600

    Add nav routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Components/shared/Home';
+import About from './Components/shared/About';
+import NoMatch from './Components/shared/NoMatch';
+import Navbar from './Components/shared/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// nav routes
+const App = () => (
+  <>
+    <Navbar />
+    <Switch>
+    <Route exact path="/" component={ Home } />
+    <Route exact path="/about" component={ About } />
+    {/* MAKE SURE NoMatch IS THE LAST ROUTE */}
+    <Route component={NoMatch} />
+    </Switch>
+  </>
+)
 
 export default App;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  blog = Blog.create(
+  title: Faker::Dessert.topping,
+  category: Faker::Dessert.variety
+  )
+  3.times do
+    Post.create(
+      title: Faker::Space.moon,
+      author: Faker::Space.nebula,
+      body: Faker::Space.agency,
+      blog_id: blog.id 
+    )
+  end
+end
+
+puts "Data Seeded"
