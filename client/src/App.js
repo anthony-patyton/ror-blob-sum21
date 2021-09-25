cb45311a509a0536dd1c2bcae356fdc2673c8bd7
Anthony Payton <paytonanthony2020@gmail.com>
2021-09-24 22:35:51 -0600

    fixed node_modules & error with webpack

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import { Route, Switch } from 'react-router-dom';
-import Home from './Components/shared/Home';
-import About from './Components/shared/About';
-import NoMatch from './Components/shared/NoMatch';
-import Navbar from './Components/shared/Navbar';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
+import NoMatch from './components/shared/NoMatch';
+import Navbar from './components/shared/Navbar';
+import Container from '@mui/material/Container';
 
 // nav routes
 const App = () => (
   <>
     <Navbar />
-    <Switch>
-    <Route exact path="/" component={ Home } />
-    <Route exact path="/about" component={ About } />
-    {/* MAKE SURE NoMatch IS THE LAST ROUTE */}
-    <Route component={NoMatch} />
-    </Switch>
+    <Container>
+      <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/about" component={ About } />
+      <Route component={NoMatch} />
+      </Switch>
+    </Container>
   </>
 )
 
